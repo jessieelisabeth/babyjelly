0871e865c549f9577e09a5cd813f96090c114074
Jess <villagranjessie03@gmail.com>
2026-09-09 23:50:27 -0600

    add append only log and replay

--- babyjelly.js
+++ babyjelly.js
@@ -3,13 +3,32 @@ const fs = require('fs');
 let babyjellyDB = new Map();
 try {
     
-    if (fs.existsSync('babyjelly.json')) {
-        const data = fs.readFileSync('babyjelly.json', 'utf8');
-        const parsedData = JSON.parse(data);
-        babyjellyDB = new Map(parsedData);
-        console.log('babyjellyDB loaded from babyjelly.json');
+    if (fs.existsSync('babyjelly.jsonl')) {
+        const data = fs.readFileSync('babyjelly.jsonl', 'utf8');
+
+const lines = data.split('\n'); // split the data into lines
+    for (const line of lines) {
+    if (line === '') { // skip empty lines
+        continue;
+    }
+
+    const record = JSON.parse(line);
+
+    switch (record[0]) { // the past
+        case 'set':
+            babyjellyDB.set(record[1], record[2]);
+            break;
+        case 'delete':
+            babyjellyDB.delete(record[1]);
+            break;
+        default:
+            console.log(`what are you waffling on about?: ${record[0]}`);
+    }
+}
+
+        console.log('babyjellyDB loaded from babyjelly.jsonl');
     } else {
-        console.log('babyjelly.json does not exist yet. Starting with an empty database.');
+        console.log('babyjelly.jsonl does not exist yet. Starting with an empty database.');
     }
 } catch (err) {
     console.error('what are you waffling on about?:', err);
@@ -21,7 +40,7 @@ const rl = readline.createInterface({
   prompt: 'babyjelly> '
 });
 
-
+// the present
 rl.prompt();
 
 rl.on('line', (line) => {
@@ -30,18 +49,18 @@ rl.on('line', (line) => {
 
     switch (command) {
         case 'set':
+            fs.appendFileSync('babyjelly.jsonl', JSON.stringify(['set', args[0], args[1]]) + '\n');
             babyjellyDB.set(args[0], args[1]);
             break;
         case 'get':
             console.log(babyjellyDB.get(args[0]));
             break;
         case 'delete':
+            fs.appendFileSync('babyjelly.jsonl', JSON.stringify(['delete', args[0]]) + '\n');
             babyjellyDB.delete(args[0]);
             break;
         case 'save':
-            const text = JSON.stringify(Array.from(babyjellyDB));
-            fs.writeFileSync('babyjelly.json', text);
-            console.log('babyjellyDB saved to babyjelly.json');
+            console.log('babyjelly is already up to date');
             break;
         case 'exit':
             rl.close();
